6244be3300854d7ab2d8c32b5926bbcc1cc1033b
shujat khan <shujatullahkhan999@gmail.com>
2021-01-24 12:10:26 -0800

    practise

---
+++ app.js
@@ -0,0 +1,191 @@
+
+
+// var a = prompt("Enter your name");
+// var b = a.toUpperCase();
+// document.write(b);
+
+
+
+// var add = prompt("Enter Your name", "Your name is...");
+// var b = add.toLowerCase();
+// document.write(b);
+
+
+// var mobile = prompt("Enter your mobile details...");
+
+// var b = mobile.length
+
+// document.write(b);
+
+
+
+// var a = "pakistani";
+
+// var b = a.indexOf("n");
+// document.write(b);
+
+
+// var a = prompt("Enter your name");
+
+// if(a === "shujat"){
+
+//     document.write("Correct user name");
+
+// }
+
+// else if(a === "@"){
+
+//     document.write("enter valid username");
+
+// }
+
+// else if(a === "!"){
+
+//     document.write("enter valid username");
+
+// }
+
+// else{
+
+//     document.write("none");
+// }
+
+
+// var str = "the quick brown fox jumps over the lazy dog";
+
+// var b = str.indexOf("the");
+// var c = str.lastIndexOf("the");
+// document.write(b + c);
+
+
+
+// var str = "pakistani";
+// var b = str.charAt(3);
+// document.write(b);
+
+
+// var city = "hyderabad";
+// var replace = city.replace("hyder", "Islam");
+// document.write(replace);
+
+
+
+// var str = "Ali and sami are best friends. They play cricket and football together";
+
+// var replace = str.replace(/and/g, "&");
+
+// document.write(replace);
+
+
+// var number = parseInt(prompt("Enter Your Number"));
+// var result = Math.round(number);
+// document.write(result);
+
+
+// var sum = Math.floor(3.45214);
+// document.write(sum); 
+
+
+// var number = +prompt("Enter Your Number"); 
+// var sum = Math.ceil(number);
+// document.write(sum);
+
+
+
+
+// var a = -2.678;
+// var b = Math.round(a);
+// document.write(b);
+
+
+// var sum = prompt("Enter negative value");
+// var a = Math.ceil(sum);
+// document.write(a);
+
+
+// var number = -4.4545;
+// var a = Math.floor(number);
+// document.write(a);
+
+
+
+// var a = Math.floor(Math.random() * 2) + 1;
+
+// if(a === 1){
+
+//     document.write(a + "head");
+// }
+
+// else{
+
+//     document.write(a + "tails");
+// }
+
+
+// var a = +prompt("Enter your number");
+// var b = Math.floor(Math.random() * 10) + 1;
+
+// if(a === b){
+
+//     document.write("Correct Answer");
+
+// }
+
+// else{
+
+//     document.write("pls try again");
+// }
+
+
+
+
+// var a = Number(prompt("Enter your weight", "you weight is ..."));
+// alert("This is your body weight" + " " + a);
+
+
+
+// var a = 472;
+// var b = 10;
+// var d  = a.toString();
+// document.write(d + b);
+
+
+// var a = "50";
+// var b = 10
+// var c = Number(a);
+// document.write(c + b)
+
+
+// var num = 35.36;
+// var a = num.toString();
+// var b = a.slice(0,2);
+// var d = a.slice(3,5)
+// var e = b + d
+// document.write(a + "<br>" + e);
+
+
+// var percentage = (30 / 45) * 100;
+// var result = percentage.toFixed(2)
+// document.write(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
